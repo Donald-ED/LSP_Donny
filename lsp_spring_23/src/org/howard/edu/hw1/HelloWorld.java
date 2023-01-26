5358594821aa167cfddc79402d70e5daf6a19ada
Donald Echefu <donaldechefu@gmail.com>
2023-01-26 12:32:43 -0500

    This is my first commit to Github.

---
+++ lsp_spring_23/src/org/howard/edu/hw1/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.howard.edu.hw1;
+
+/**
+ * @author Donald Echefu
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
